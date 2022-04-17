428cc14b71ea7d719dddaa97aa2ba5f6cdaf72de
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-04-17 10:01:14 +0530

    UC2

---
+++ Mood Analyser-2/src/com/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,14 @@
+package com.moodanalyser;
+
+public class MoodAnalyser {
+
+	public MoodAnalyser(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String analyseMood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Mood Analyser-2/src/com/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,26 @@
+package com.moodanalyser;
+
+import org.junit.Test;
+import org.junit.Assert;
+public class MoodAnalyserTest {
+    
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("Sad", mood);
+    }
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("Happy",mood);
+    }
+
+    @Test
+    public void givenNullMood_ShouldReturnWarning(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("Message is empty.",mood);
+    }
+}
