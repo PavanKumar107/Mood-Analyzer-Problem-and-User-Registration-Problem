c110eab7c01dd79ef2f84c313bda224dc3b0e3b4
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-04-17 10:01:42 +0530

    UC3

---
+++ Mood Analyser-3/src/com/moodanalyser/MoodAnalysisException.java
@@ -0,0 +1,13 @@
+package com.moodanalyser;
+public class MoodAnalysisException extends Exception {
+
+    enum ExceptionType {
+        NULL, INVALID_ENTRY
+    }
+
+    ExceptionType type;
+    public MoodAnalysisException(ExceptionType type, String msg) {
+        super(msg);
+        this.type = type;
+    }
+}
